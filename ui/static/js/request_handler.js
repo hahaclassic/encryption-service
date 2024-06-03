057d30ed11083dd317997e158a249deab34c54ef
hahaclassic <gavrilyukvlad221@gmail.com>
2024-06-03 22:57:26 +0300

    deleted useless comments

--- ui/static/js/request_handler.js
+++ ui/static/js/request_handler.js
@@ -2,16 +2,11 @@
 
 
 function sendJSON(OperationType) {
-    // с помощью jQuery обращаемся к элементам на странице по их именам
     let OriginalMessage = document.querySelector('#OriginalMessage');
     let ConvertedMessage = document.querySelector('#ConvertedMessage');
     let Key = document.querySelector('#Key');
 
-    // а вот сюда мы поместим ответ от сервера
-    //let result = document.querySelector('#ConvertedMessage');
-    // создаём новый экземпляр запроса XHR
     let xhr = new XMLHttpRequest();
-    // адрес, куда мы отправим нашу JSON-строку
 
     const title = document.querySelector('#title')
     let url
@@ -31,17 +26,11 @@ function sendJSON(OperationType) {
 
     }
 
-
-    // открываем соединение
     xhr.open("POST", url, true);
-    // устанавливаем заголовок — выбираем тип контента, который отправится на сервер, в нашем случае мы явно пишем, что это JSON
     xhr.setRequestHeader("Content-Type", "application/json");
 
-    // когда придёт ответ на наше обращение к серверу, мы его обработаем здесь
     xhr.onreadystatechange = function () {
-        // если запрос принят и сервер ответил, что всё в порядке
         if (xhr.readyState === 4 && xhr.status === 200) {
-        // выводим то, что ответил нам сервер — так мы убедимся, что данные он получил правильно
             if (OperationType === 'GetRandomKey') {
                 ConvertedMessage.value = ''
                 Key.value = JSON.parse(this.responseText).key;
@@ -53,7 +42,6 @@ function sendJSON(OperationType) {
         }
     };
    
-    // преобразуем наши данные JSON в строку
     // OperationType    string `json:"operation_type"`
     // OriginalMessage  string `json:"original"`
     // ConvertedMessage string `json:"converted"`
@@ -64,25 +52,8 @@ function sendJSON(OperationType) {
      "original": OriginalMessage.value, "converted": "", 
     "key": Key.value, "language": "eng"});
 
-    // когда всё готово, отправляем JSON на сервер
     xhr.send(data);
 }
-
-// document.querySelector("#GetRandomKey").onclick = function() {
-//     sendJSON('GetRandomKey')
-//     sendJSON('Encrypt')
-// }
-
-// document.querySelector("#EncryptButton").onclick = function() {
-//     sendJSON('Encrypt')
-// }
-
-// document.querySelector("#DecryptButton").onclick = function() {
-//     sendJSON('Decrypt')
-// }
-
-
-
 
 function sendRequest() {
     let OriginLabel = document.querySelector('#originLabel');
